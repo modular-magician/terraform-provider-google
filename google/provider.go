8b2a80a59f6f3ad3415c8d1cbb7514435340197a
Ali Khan <mohhamed.khan@gmail.com>
2019-10-03 19:55:15 +0000

    App Engine Service Split Traffic resource
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider.go
+++ google/provider.go
@@ -437,9 +437,9 @@ func Provider() terraform.ResourceProvider {
 	return provider
 }
 
-// Generated resources: 80
+// Generated resources: 81
 // Generated IAM resources: 27
-// Total generated resources: 107
+// Total generated resources: 108
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -453,6 +453,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_access_context_manager_service_perimeter":  resourceAccessContextManagerServicePerimeter(),
 			"google_app_engine_domain_mapping":                 resourceAppEngineDomainMapping(),
 			"google_app_engine_firewall_rule":                  resourceAppEngineFirewallRule(),
+			"google_app_engine_service_split_traffic":          resourceAppEngineServiceSplitTraffic(),
 			"google_app_engine_standard_app_version":           resourceAppEngineStandardAppVersion(),
 			"google_bigquery_dataset":                          resourceBigQueryDataset(),
 			"google_bigquery_data_transfer_config":             resourceBigqueryDataTransferConfig(),

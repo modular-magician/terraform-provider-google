159e97a51c5ce6889457fa734c37f9dbfc83a1f5
Modular Magician <magic-modules@google.com>
2021-11-12 07:13:05 +0000

    New generated code for MM PR 5242.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider.go
+++ google/provider.go
@@ -803,9 +803,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 214
+// Generated resources: 216
 // Generated IAM resources: 87
-// Total generated resources: 301
+// Total generated resources: 303
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -951,6 +951,8 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_compute_url_map":                                       resourceComputeUrlMap(),
 			"google_compute_vpn_tunnel":                                    resourceComputeVpnTunnel(),
 			"google_compute_target_grpc_proxy":                             resourceComputeTargetGrpcProxy(),
+			"google_compute_accelerator_type":                              resourceComputeAcceleratorType(),
+			"google_compute_global_operation":                              resourceComputeGlobalOperation(),
 			"google_container_analysis_note":                               resourceContainerAnalysisNote(),
 			"google_container_analysis_occurrence":                         resourceContainerAnalysisOccurrence(),
 			"google_data_catalog_entry_group":                              resourceDataCatalogEntryGroup(),

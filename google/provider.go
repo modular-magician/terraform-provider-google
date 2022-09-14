14b1d065f08d76a495a8408d4621a857ae703293
Modular Magician <magic-modules@google.com>
2022-09-14 18:17:09 +0000

    New generated code for MM PR 6548.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider.go
+++ google/provider.go
@@ -888,9 +888,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 233
+// Generated resources: 234
 // Generated IAM resources: 138
-// Total generated resources: 371
+// Total generated resources: 372
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -1172,6 +1172,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_identity_platform_tenant":                              resourceIdentityPlatformTenant(),
 			"google_kms_key_ring":                                          resourceKMSKeyRing(),
 			"google_kms_crypto_key":                                        resourceKMSCryptoKey(),
+			"google_kms_crypto_key_version":                                resourceKMSCryptoKeyVersion(),
 			"google_kms_key_ring_import_job":                               resourceKMSKeyRingImportJob(),
 			"google_kms_secret_ciphertext":                                 resourceKMSSecretCiphertext(),
 			"google_logging_metric":                                        resourceLoggingMetric(),

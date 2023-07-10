ba77f87273887452fd3a2a0f2615e5a3c38400be
Modular Magician <magic-modules@google.com>
2023-07-10 16:57:37 +0000

    New generated code for MM PR 8257.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/clouddeploy/resource_clouddeploy_delivery_pipeline_generated_test.go
+++ google/services/clouddeploy/resource_clouddeploy_delivery_pipeline_generated_test.go
@@ -16,13 +16,13 @@
 //
 // ----------------------------------------------------------------------------
 
-package google
+package clouddeploy_test
 
 import (
 	"context"
 	"fmt"
 	dcl "github.com/GoogleCloudPlatform/declarative-resource-client-library/dcl"
-	dataplex "github.com/GoogleCloudPlatform/declarative-resource-client-library/services/google/dataplex"
+	clouddeploy "github.com/GoogleCloudPlatform/declarative-resource-client-library/services/google/clouddeploy"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"strings"
@@ -33,33 +33,33 @@ import (
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
 
-func TestAccDataplexZone_BasicZone(t *testing.T) {
+func TestAccClouddeployDeliveryPipeline_DeliveryPipeline(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
 		"project_name":  envvar.GetTestProjectFromEnv(),
 		"region":        envvar.GetTestRegionFromEnv(),
-		"random_suffix": RandString(t, 10),
+		"random_suffix": acctest.RandString(t, 10),
 	}
 
-	VcrTest(t, resource.TestCase{
+	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckDataplexZoneDestroyProducer(t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckClouddeployDeliveryPipelineDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataplexZone_BasicZone(context),
+				Config: testAccClouddeployDeliveryPipeline_DeliveryPipeline(context),
 			},
 			{
-				ResourceName:      "google_dataplex_zone.primary",
+				ResourceName:      "google_clouddeploy_delivery_pipeline.primary",
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccDataplexZone_BasicZoneUpdate0(context),
+				Config: testAccClouddeployDeliveryPipeline_DeliveryPipelineUpdate0(context),
 			},
 			{
-				ResourceName:      "google_dataplex_zone.primary",
+				ResourceName:      "google_clouddeploy_delivery_pipeline.primary",
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
@@ -67,123 +67,119 @@ func TestAccDataplexZone_BasicZone(t *testing.T) {
 	})
 }
 
-func testAccDataplexZone_BasicZone(context map[string]interface{}) string {
+func testAccClouddeployDeliveryPipeline_DeliveryPipeline(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_dataplex_zone" "primary" {
-  discovery_spec {
-    enabled = false
-  }
-
-  lake     = google_dataplex_lake.basic.name
+resource "google_clouddeploy_delivery_pipeline" "primary" {
   location = "%{region}"
-  name     = "tf-test-zone%{random_suffix}"
+  name     = "tf-test-pipeline%{random_suffix}"
 
-  resource_spec {
-    location_type = "MULTI_REGION"
+  annotations = {
+    my_first_annotation = "example-annotation-1"
+
+    my_second_annotation = "example-annotation-2"
   }
 
-  type         = "RAW"
-  description  = "Zone for DCL"
-  display_name = "Zone for DCL"
-  labels       = {}
-  project      = "%{project_name}"
-}
-
-resource "google_dataplex_lake" "basic" {
-  location     = "%{region}"
-  name         = "tf-test-lake%{random_suffix}"
-  description  = "Lake for DCL"
-  display_name = "Lake for DCL"
+  description = "basic description"
 
   labels = {
-    my-lake = "exists"
+    my_first_label = "example-label-1"
+
+    my_second_label = "example-label-2"
   }
 
   project = "%{project_name}"
+
+  serial_pipeline {
+    stages {
+      profiles  = ["example-profile-one", "example-profile-two"]
+      target_id = "example-target-one"
+    }
+
+    stages {
+      profiles  = []
+      target_id = "example-target-two"
+    }
+  }
 }
 
 
 `, context)
 }
 
-func testAccDataplexZone_BasicZoneUpdate0(context map[string]interface{}) string {
+func testAccClouddeployDeliveryPipeline_DeliveryPipelineUpdate0(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_dataplex_zone" "primary" {
-  discovery_spec {
-    enabled = false
-  }
-
-  lake     = google_dataplex_lake.basic.name
+resource "google_clouddeploy_delivery_pipeline" "primary" {
   location = "%{region}"
-  name     = "tf-test-zone%{random_suffix}"
+  name     = "tf-test-pipeline%{random_suffix}"
 
-  resource_spec {
-    location_type = "MULTI_REGION"
+  annotations = {
+    my_second_annotation = "updated-example-annotation-2"
+
+    my_third_annotation = "example-annotation-3"
   }
 
-  type         = "RAW"
-  description  = "Zone for DCL Updated"
-  display_name = "Zone for DCL"
+  description = "updated description"
 
   labels = {
-    updated_label = "exists"
+    my_second_label = "updated-example-label-2"
+
+    my_third_label = "example-label-3"
   }
 
   project = "%{project_name}"
-}
 
-resource "google_dataplex_lake" "basic" {
-  location     = "%{region}"
-  name         = "tf-test-lake%{random_suffix}"
-  description  = "Lake for DCL"
-  display_name = "Lake for DCL"
+  serial_pipeline {
+    stages {
+      profiles  = ["new-example-profile"]
+      target_id = "example-target-two"
+    }
 
-  labels = {
-    my-lake = "exists"
+    stages {
+      profiles  = ["example-profile-four", "example-profile-five"]
+      target_id = "example-target-three"
+    }
   }
 
-  project = "%{project_name}"
+  suspended = true
 }
 
 
 `, context)
 }
 
-func testAccCheckDataplexZoneDestroyProducer(t *testing.T) func(s *terraform.State) error {
+func testAccCheckClouddeployDeliveryPipelineDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
-			if rs.Type != "rs.google_dataplex_zone" {
+			if rs.Type != "rs.google_clouddeploy_delivery_pipeline" {
 				continue
 			}
 			if strings.HasPrefix(name, "data.") {
 				continue
 			}
 
-			config := GoogleProviderConfig(t)
+			config := acctest.GoogleProviderConfig(t)
 
 			billingProject := ""
 			if config.BillingProject != "" {
 				billingProject = config.BillingProject
 			}
 
-			obj := &dataplex.Zone{
-				Lake:        dcl.String(rs.Primary.Attributes["lake"]),
+			obj := &clouddeploy.DeliveryPipeline{
 				Location:    dcl.String(rs.Primary.Attributes["location"]),
 				Name:        dcl.String(rs.Primary.Attributes["name"]),
-				Type:        dataplex.ZoneTypeEnumRef(rs.Primary.Attributes["type"]),
 				Description: dcl.String(rs.Primary.Attributes["description"]),
-				DisplayName: dcl.String(rs.Primary.Attributes["display_name"]),
 				Project:     dcl.StringOrNil(rs.Primary.Attributes["project"]),
+				Suspended:   dcl.Bool(rs.Primary.Attributes["suspended"] == "true"),
 				CreateTime:  dcl.StringOrNil(rs.Primary.Attributes["create_time"]),
-				State:       dataplex.ZoneStateEnumRef(rs.Primary.Attributes["state"]),
+				Etag:        dcl.StringOrNil(rs.Primary.Attributes["etag"]),
 				Uid:         dcl.StringOrNil(rs.Primary.Attributes["uid"]),
 				UpdateTime:  dcl.StringOrNil(rs.Primary.Attributes["update_time"]),
 			}
 
-			client := transport_tpg.NewDCLDataplexClient(config, config.UserAgent, billingProject, 0)
-			_, err := client.GetZone(context.Background(), obj)
+			client := transport_tpg.NewDCLClouddeployClient(config, config.UserAgent, billingProject, 0)
+			_, err := client.GetDeliveryPipeline(context.Background(), obj)
 			if err == nil {
-				return fmt.Errorf("google_dataplex_zone still exists %v", obj)
+				return fmt.Errorf("google_clouddeploy_delivery_pipeline still exists %v", obj)
 			}
 		}
 		return nil

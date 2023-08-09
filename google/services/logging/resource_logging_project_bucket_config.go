81976a934fd38f9100397ed7dcc7cf9a3cb07d0d
Modular Magician <magic-modules@google.com>
2023-08-09 21:31:34 +0000

    New generated code for MM PR 8497.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/logging/resource_logging_project_bucket_config.go
+++ google/services/logging/resource_logging_project_bucket_config.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/customdiff"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -135,6 +136,10 @@ func ResourceLoggingProjectBucketConfig() *schema.Resource {
 		},
 		Schema:        loggingProjectBucketConfigSchema,
 		UseJSONNumber: true,
+
+		CustomizeDiff: customdiff.All(
+			tpgresource.DefaultProviderProject,
+		),
 	}
 }
 

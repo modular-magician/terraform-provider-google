fb33aa1ebd701b6ad797f3916879a7f578f8e348
Modular Magician <magic-modules@google.com>
2023-08-14 22:35:15 +0000

    New generated code for MM PR 8623.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider.go
+++ google/provider/provider.go
@@ -965,9 +965,9 @@ func DatasourceMapWithErrors() (map[string]*schema.Resource, error) {
 		})
 }
 
-// Generated resources: 307
+// Generated resources: 308
 // Generated IAM resources: 204
-// Total generated resources: 511
+// Total generated resources: 512
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -1068,6 +1068,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_certificate_manager_certificate_map":                     certificatemanager.ResourceCertificateManagerCertificateMap(),
 			"google_certificate_manager_certificate_map_entry":               certificatemanager.ResourceCertificateManagerCertificateMapEntry(),
 			"google_certificate_manager_dns_authorization":                   certificatemanager.ResourceCertificateManagerDnsAuthorization(),
+			"google_certificate_manager_trust_config":                        certificatemanager.ResourceCertificateManagerTrustConfig(),
 			"google_cloud_asset_folder_feed":                                 cloudasset.ResourceCloudAssetFolderFeed(),
 			"google_cloud_asset_organization_feed":                           cloudasset.ResourceCloudAssetOrganizationFeed(),
 			"google_cloud_asset_project_feed":                                cloudasset.ResourceCloudAssetProjectFeed(),

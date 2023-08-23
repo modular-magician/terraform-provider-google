8566da17163059bb54ac923be0b4e50aaf9de586
Modular Magician <magic-modules@google.com>
2023-08-23 21:42:01 +0000

    New generated code for MM PR 8699.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/config_test_utils.go
+++ google/config_test_utils.go
@@ -31,6 +31,7 @@ func configureTestBasePaths(c *transport_tpg.Config, url string) {
 	c.AppEngineBasePath = url
 	c.ArtifactRegistryBasePath = url
 	c.BeyondcorpBasePath = url
+	c.BiglakeBasePath = url
 	c.BigQueryBasePath = url
 	c.BigqueryAnalyticsHubBasePath = url
 	c.BigqueryConnectionBasePath = url

c9739b7b54ec394bc6f462168c0b3d9cbc87ebdd
Modular Magician <magic-modules@google.com>
2024-01-30 15:07:16 +0000

    New generated code for MM PR 9812.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -382,9 +382,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 371
+// Generated resources: 372
 // Generated IAM resources: 222
-// Total generated resources: 593
+// Total generated resources: 594
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                         accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                   accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -824,6 +824,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_monitoring_slo":                                          monitoring.ResourceMonitoringSlo(),
 	"google_monitoring_uptime_check_config":                          monitoring.ResourceMonitoringUptimeCheckConfig(),
 	"google_netapp_volume":                                           netapp.ResourceNetappVolume(),
+	"google_netapp_volume_snapshot":                                  netapp.ResourceNetappVolumeSnapshot(),
 	"google_netapp_active_directory":                                 netapp.ResourceNetappactiveDirectory(),
 	"google_netapp_backup_policy":                                    netapp.ResourceNetappbackupPolicy(),
 	"google_netapp_backup_vault":                                     netapp.ResourceNetappbackupVault(),

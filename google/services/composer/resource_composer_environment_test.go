24a40ff29a1635c9ff1a5fe51a1fc3d9d99e49fb
Modular Magician <magic-modules@google.com>
2024-02-29 20:36:39 +0000

    New generated code for MM PR 10078.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/composer/resource_composer_environment_test.go
+++ google/services/composer/resource_composer_environment_test.go
@@ -1174,7 +1174,7 @@ resource "google_composer_environment" "test" {
       }
     }
     software_config {
-      image_version = "composer-1-airflow-2.3"
+      image_version = "composer-2.6.2-airflow-2.5.3"
     }
   }
 }

a4cf47836c9d8cf2f51c1c2264033e58b743c903
Modular Magician <magic-modules@google.com>
2024-09-11 08:59:04 +0000

    New generated code for MM PR 11604.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -430,9 +430,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 453
+// Generated resources: 454
 // Generated IAM resources: 258
-// Total generated resources: 711
+// Total generated resources: 712
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -698,6 +698,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_container_attached_cluster":                                          containerattached.ResourceContainerAttachedCluster(),
 	"google_billing_project_info":                                                corebilling.ResourceCoreBillingProjectInfo(),
 	"google_database_migration_service_connection_profile":                       databasemigrationservice.ResourceDatabaseMigrationServiceConnectionProfile(),
+	"google_database_migration_service_migration_job":                            databasemigrationservice.ResourceDatabaseMigrationServiceMigrationJob(),
 	"google_database_migration_service_private_connection":                       databasemigrationservice.ResourceDatabaseMigrationServicePrivateConnection(),
 	"google_data_catalog_entry":                                                  datacatalog.ResourceDataCatalogEntry(),
 	"google_data_catalog_entry_group":                                            datacatalog.ResourceDataCatalogEntryGroup(),

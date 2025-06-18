e72f1e5f6f10f440659df438938da25f9092c533
Modular Magician <magic-modules@google.com>
2025-06-18 11:34:41 +0000

    New generated code for MM PR 14286.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -113,6 +113,7 @@ import (
 	"github.com/hashicorp/terraform-provider-google/google/services/memorystore"
 	"github.com/hashicorp/terraform-provider-google/google/services/migrationcenter"
 	"github.com/hashicorp/terraform-provider-google/google/services/mlengine"
+	"github.com/hashicorp/terraform-provider-google/google/services/modelarmor"
 	"github.com/hashicorp/terraform-provider-google/google/services/monitoring"
 	"github.com/hashicorp/terraform-provider-google/google/services/netapp"
 	"github.com/hashicorp/terraform-provider-google/google/services/networkconnectivity"
@@ -542,9 +543,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 620
+// Generated resources: 621
 // Generated IAM resources: 309
-// Total generated resources: 929
+// Total generated resources: 930
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1201,6 +1202,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_migration_center_group":                                              migrationcenter.ResourceMigrationCenterGroup(),
 	"google_migration_center_preference_set":                                     migrationcenter.ResourceMigrationCenterPreferenceSet(),
 	"google_ml_engine_model":                                                     mlengine.ResourceMLEngineModel(),
+	"google_model_armor_template":                                                modelarmor.ResourceModelArmorTemplate(),
 	"google_monitoring_alert_policy":                                             monitoring.ResourceMonitoringAlertPolicy(),
 	"google_monitoring_service":                                                  monitoring.ResourceMonitoringGenericService(),
 	"google_monitoring_group":                                                    monitoring.ResourceMonitoringGroup(),
@@ -1708,6 +1710,7 @@ func UseGeneratedProducts() {
 	var _ = memorystore.ProductName
 	var _ = migrationcenter.ProductName
 	var _ = mlengine.ProductName
+	var _ = modelarmor.ProductName
 	var _ = monitoring.ProductName
 	var _ = netapp.ProductName
 	var _ = networkconnectivity.ProductName

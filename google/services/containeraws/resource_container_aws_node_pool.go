201931b428710b702c3ddf00681349468a02c512
Modular Magician <magic-modules@google.com>
2025-10-01 23:32:11 +0000

    Old generated code for MM PR 15019.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/containeraws/resource_container_aws_node_pool.go
+++ google/services/containeraws/resource_container_aws_node_pool.go
@@ -60,16 +60,6 @@ func ResourceContainerAwsNodePool() *schema.Resource {
 			Version: 1,
 			SchemaFunc: func() map[string]*schema.Schema {
 				return map[string]*schema.Schema{
-					"name": {
-						Type:              schema.TypeString,
-						RequiredForImport: true,
-						Description:       "The name of this resource.",
-					},
-					"cluster": {
-						Type:              schema.TypeString,
-						RequiredForImport: true,
-						Description:       "The awsCluster for the resource",
-					},
 					"location": {
 						Type:              schema.TypeString,
 						RequiredForImport: true,
@@ -79,6 +69,16 @@ func ResourceContainerAwsNodePool() *schema.Resource {
 						Type:              schema.TypeString,
 						OptionalForImport: true,
 						Description:       "The project for the resource",
+					},
+					"name": {
+						Type:              schema.TypeString,
+						RequiredForImport: true,
+						Description:       "The name of this resource.",
+					},
+					"cluster": {
+						Type:              schema.TypeString,
+						RequiredForImport: true,
+						Description:       "The awsCluster for the resource",
 					},
 				}
 			},

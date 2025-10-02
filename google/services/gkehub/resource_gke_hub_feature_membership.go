fcb429eecf14825cb3b46544d968df179f7fc666
Modular Magician <magic-modules@google.com>
2025-10-02 17:11:59 +0000

    Old generated code for MM PR 15291.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/gkehub/resource_gke_hub_feature_membership.go
+++ google/services/gkehub/resource_gke_hub_feature_membership.go
@@ -55,6 +55,11 @@ func ResourceGkeHubFeatureMembership() *schema.Resource {
 			Version: 1,
 			SchemaFunc: func() map[string]*schema.Schema {
 				return map[string]*schema.Schema{
+					"location": {
+						Type:              schema.TypeString,
+						RequiredForImport: true,
+						Description:       "The location of the feature",
+					},
 					"membership": {
 						Type:              schema.TypeString,
 						RequiredForImport: true,
@@ -69,11 +74,6 @@ func ResourceGkeHubFeatureMembership() *schema.Resource {
 						Type:              schema.TypeString,
 						RequiredForImport: true,
 						Description:       "The name of the feature",
-					},
-					"location": {
-						Type:              schema.TypeString,
-						RequiredForImport: true,
-						Description:       "The location of the feature",
 					},
 				}
 			},

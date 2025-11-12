057db98468d21120cbdb4f2e02ee301698436314
Modular Magician <magic-modules@google.com>
2025-11-12 17:15:43 +0000

    New generated code for MM PR 15580.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -580,9 +580,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 674
+// Generated resources: 675
 // Generated IAM resources: 318
-// Total generated resources: 992
+// Total generated resources: 993
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1372,6 +1372,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_oracle_database_cloud_exadata_infrastructure":                        oracledatabase.ResourceOracleDatabaseCloudExadataInfrastructure(),
 	"google_oracle_database_cloud_vm_cluster":                                    oracledatabase.ResourceOracleDatabaseCloudVmCluster(),
 	"google_oracle_database_db_system":                                           oracledatabase.ResourceOracleDatabaseDbSystem(),
+	"google_oracle_database_exascale_db_storage_vault":                           oracledatabase.ResourceOracleDatabaseExascaleDbStorageVault(),
 	"google_oracle_database_odb_network":                                         oracledatabase.ResourceOracleDatabaseOdbNetwork(),
 	"google_oracle_database_odb_subnet":                                          oracledatabase.ResourceOracleDatabaseOdbSubnet(),
 	"google_org_policy_custom_constraint":                                        orgpolicy.ResourceOrgPolicyCustomConstraint(),

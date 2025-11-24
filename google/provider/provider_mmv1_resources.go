7965e14e6ce59bc9d03ff21c2e3df1fd6d51bd14
Modular Magician <magic-modules@google.com>
2025-11-24 22:39:19 +0000

    New generated code for MM PR 15674.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -584,9 +584,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 689
+// Generated resources: 690
 // Generated IAM resources: 324
-// Total generated resources: 1013
+// Total generated resources: 1014
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1369,6 +1369,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_network_services_multicast_domain":                                   networkservices.ResourceNetworkServicesMulticastDomain(),
 	"google_network_services_multicast_domain_activation":                        networkservices.ResourceNetworkServicesMulticastDomainActivation(),
 	"google_network_services_multicast_domain_group":                             networkservices.ResourceNetworkServicesMulticastDomainGroup(),
+	"google_network_services_multicast_group_range":                              networkservices.ResourceNetworkServicesMulticastGroupRange(),
 	"google_network_services_multicast_producer_association":                     networkservices.ResourceNetworkServicesMulticastProducerAssociation(),
 	"google_network_services_service_binding":                                    networkservices.ResourceNetworkServicesServiceBinding(),
 	"google_network_services_tcp_route":                                          networkservices.ResourceNetworkServicesTcpRoute(),

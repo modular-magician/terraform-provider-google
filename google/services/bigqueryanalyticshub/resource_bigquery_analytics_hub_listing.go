f055fc476c6ba085f60116b3a5c2999d2a02798a
Modular Magician <magic-modules@google.com>
2026-01-14 23:06:16 +0000

    New generated code for MM PR 16021.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/bigqueryanalyticshub/resource_bigquery_analytics_hub_listing.go
+++ google/services/bigqueryanalyticshub/resource_bigquery_analytics_hub_listing.go
@@ -126,10 +126,11 @@ func ResourceBigqueryAnalyticsHubListing() *schema.Resource {
 				Description: `The ID of the listing. Must contain only Unicode letters, numbers (0-9), underscores (_). Should not use characters that require URL-escaping, or characters outside of ASCII, spaces.`,
 			},
 			"location": {
-				Type:        schema.TypeString,
-				Required:    true,
-				ForceNew:    true,
-				Description: `The name of the location this data exchange listing.`,
+				Type:             schema.TypeString,
+				Required:         true,
+				ForceNew:         true,
+				DiffSuppressFunc: tpgresource.CaseDiffSuppress,
+				Description:      `The name of the location this data exchange listing.`,
 			},
 			"allow_only_metadata_sharing": {
 				Type:        schema.TypeBool,
@@ -140,7 +141,6 @@ func ResourceBigqueryAnalyticsHubListing() *schema.Resource {
 			"bigquery_dataset": {
 				Type:        schema.TypeList,
 				Optional:    true,
-				ForceNew:    true,
 				Description: `Shared dataset i.e. BigQuery dataset source.`,
 				MaxItems:    1,
 				Elem: &schema.Resource{
@@ -683,6 +683,12 @@ func resourceBigqueryAnalyticsHubListingUpdate(d *schema.ResourceData, meta inte
 	} else if v, ok := d.GetOkExists("categories"); !tpgresource.IsEmptyValue(reflect.ValueOf(v)) && (ok || !reflect.DeepEqual(v, categoriesProp)) {
 		obj["categories"] = categoriesProp
 	}
+	bigqueryDatasetProp, err := expandBigqueryAnalyticsHubListingBigqueryDataset(d.Get("bigquery_dataset"), d, config)
+	if err != nil {
+		return err
+	} else if v, ok := d.GetOkExists("bigquery_dataset"); !tpgresource.IsEmptyValue(reflect.ValueOf(v)) && (ok || !reflect.DeepEqual(v, bigqueryDatasetProp)) {
+		obj["bigqueryDataset"] = bigqueryDatasetProp
+	}
 	pubsubTopicProp, err := expandBigqueryAnalyticsHubListingPubsubTopic(d.Get("pubsub_topic"), d, config)
 	if err != nil {
 		return err
@@ -753,6 +759,10 @@ func resourceBigqueryAnalyticsHubListingUpdate(d *schema.ResourceData, meta inte
 		updateMask = append(updateMask, "categories")
 	}
 
+	if d.HasChange("bigquery_dataset") {
+		updateMask = append(updateMask, "bigqueryDataset")
+	}
+
 	if d.HasChange("pubsub_topic") {
 		updateMask = append(updateMask, "pubsubTopic")
 	}
@@ -781,14 +791,22 @@ func resourceBigqueryAnalyticsHubListingUpdate(d *schema.ResourceData, meta inte
 			base := parts[0]
 			query := parts[1]
 
-			query = strings.ReplaceAll(query, "%2C", ",")
-			query = strings.ReplaceAll(query, "%2c", ",")
-
 			// Replace "pubsubTopic" with "pubsubTopic.dataAffinityRegions"
 			query = strings.ReplaceAll(query, "pubsubTopic", "pubsubTopic.dataAffinityRegions")
 
-			// Re-encode commas back
-			query = strings.ReplaceAll(query, ",", "%2C")
+			url = base + "?" + query
+		}
+	}
+
+	if d.HasChange("bigquery_dataset.0.replica_locations") {
+		// Split URL into base and query parts
+		parts := strings.SplitN(url, "?", 2)
+		if len(parts) == 2 {
+			base := parts[0]
+			query := parts[1]
+
+			// Replace "bigqueryDataset" with "bigqueryDataset.replicaLocations"
+			query = strings.ReplaceAll(query, "bigqueryDataset", "bigqueryDataset.replicaLocations")
 
 			url = base + "?" + query
 		}

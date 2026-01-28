b4e7d07f973ae98fe097c0a62d7eda7cfdd943d2
Modular Magician <magic-modules@google.com>
2026-01-28 16:10:05 +0000

    New generated code for MM PR 16238.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -592,9 +592,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 709
+// Generated resources: 710
 // Generated IAM resources: 327
-// Total generated resources: 1036
+// Total generated resources: 1037
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -896,6 +896,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_compute_node_template":                                               compute.ResourceComputeNodeTemplate(),
 	"google_compute_organization_security_policy":                                compute.ResourceComputeOrganizationSecurityPolicy(),
 	"google_compute_organization_security_policy_association":                    compute.ResourceComputeOrganizationSecurityPolicyAssociation(),
+	"google_compute_organization_security_policy_rule":                           compute.ResourceComputeOrganizationSecurityPolicyRule(),
 	"google_compute_packet_mirroring":                                            compute.ResourceComputePacketMirroring(),
 	"google_compute_per_instance_config":                                         compute.ResourceComputePerInstanceConfig(),
 	"google_compute_preview_feature":                                             compute.ResourceComputePreviewFeature(),

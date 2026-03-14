8d7e3a7d94e7bb56a0a0f2e8ba82b0aa220fb707
Modular Magician <magic-modules@google.com>
2026-03-14 22:13:58 +0000

    New generated code for MM PR 16704.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/bigqueryreservation/resource_bigquery_reservation_test.go
+++ google/services/bigqueryreservation/resource_bigquery_reservation_test.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
 )
 
 func TestAccBigqueryReservation_withDisasterRecovery_update(t *testing.T) {

b78895c841583f057af7e82d6478b57f88f9dd1e
Modular Magician <magic-modules@google.com>
2026-06-03 20:43:03 +0000

    New generated code for MM PR 17802.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/ces/resource_ces_evaluation_test.go
+++ google/services/ces/resource_ces_evaluation_test.go
@@ -16,4 +16,13 @@
 // ----------------------------------------------------------------------------
 package ces_test
 
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	_ "github.com/hashicorp/terraform-provider-google/google/services/ces"
+)
+
 // TestAccCESEvaluation_update is not available in GA

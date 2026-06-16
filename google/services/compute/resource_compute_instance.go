0afba1d825520d3878ad58b68c948c69c9732983
Modular Magician <magic-modules@google.com>
2026-06-16 10:45:22 +0000

    New generated code for MM PR 17972.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_instance.go
+++ google/services/compute/resource_compute_instance.go
@@ -2380,6 +2380,8 @@ func resourceComputeInstanceUpdate(d *schema.ResourceData, meta interface{}) err
 
 				instMap["description"] = d.Get("description").(string)
 
+				stripInitializeParams(instMap)
+
 				res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
 					Config:    config,
 					Method:    "PUT",
@@ -2548,6 +2550,8 @@ func resourceComputeInstanceUpdate(d *schema.ResourceData, meta interface{}) err
 					return fmt.Errorf("Error converting params: %s", err)
 				}
 				instMap["params"] = paramsMap
+
+				stripInitializeParams(instMap)
 
 				res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
 					Config:    config,
@@ -3314,6 +3318,8 @@ func resourceComputeInstanceUpdate(d *schema.ResourceData, meta interface{}) err
 
 				instMap["canIpForward"] = d.Get("can_ip_forward").(bool)
 
+				stripInitializeParams(instMap)
+
 				res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
 					Config:    config,
 					Method:    "PUT",
@@ -3644,6 +3650,8 @@ func resourceComputeInstanceUpdate(d *schema.ResourceData, meta interface{}) err
 						return fmt.Errorf("Error converting advanced_machine_features: %s", err)
 					}
 					instMap["advancedMachineFeatures"] = featuresMap
+
+					stripInitializeParams(instMap)
 
 					res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
 						Config:    config,
